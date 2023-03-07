6eb42b0050c3e329c6563eaf15aa2464a06812e8
PlasmaUnblocked <119481238+Eminence777@users.noreply.github.com>
2023-03-06 22:25:52 -0800

    Delete cookie-clicker directory

--- cookie-clicker/goguardian-bypass.js
+++
@@ -1,6 +0,0 @@
-window.addEventListener("beforeunload", function (e) {
-  var confirmationMessage = "Are you sure you want to close?";
-
-  e.returnValue = confirmationMessage;     // Gecko, Trident, Chrome 34+
-  return confirmationMessage;              // Gecko, WebKit, Chrome <34
-});
